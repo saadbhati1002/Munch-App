bbe9f680ee10cd450141da41d05417a71af61261
saadbhati1002 <saadbhati1002@gmail.com>
2024-03-05 13:08:17 +0530

    feat: splash screen integration

--- lib/screen/splash/splash_screen.dart
+++ lib/screen/splash/splash_screen.dart
@@ -1,7 +1,14 @@
+import 'dart:convert';
+
+import 'package:app/models/user/user_model.dart';
 import 'package:app/screen/auth/login/login_screen.dart';
+import 'package:app/screen/dashboard/dashborad_screen.dart';
 import 'package:app/utility/color.dart';
+import 'package:app/utility/constant.dart';
 import 'package:app/utility/images.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -14,6 +21,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     Future.delayed(const Duration(seconds: 2)).then((value) {
+      _navigation();
       Navigator.push(
         context,
         MaterialPageRoute(
@@ -22,6 +30,18 @@ class _SplashScreenState extends State<SplashScreen> {
       );
     });
     super.initState();
+  }
+
+  _navigation() async {
+    var response = await AppConstant.getUserDetail();
+    if (response != null && response != "null") {
+      UserRes responseUser = UserRes.fromJson(jsonDecode(response));
+      AppConstant.userData = responseUser.data;
+      AppConstant.bearerToken = responseUser.data!.token!;
+      Get.to(() => const DashBoardScreen());
+    } else {
+      Get.to(() => const LoginScreen());
+    }
   }
 
   @override
